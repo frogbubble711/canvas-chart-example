cfda4f554491160f1855b61013a18656dc22085a
frogbubble711 <frogbubble711@gmail.com>
2021-02-14 09:52:09 -0400

    cleaning

--- src/assets/canvasjs.react.js
+++ src/assets/canvasjs.react.js
@@ -22,17 +22,6 @@ class CanvasJSChart extends React.Component {
   componentDidMount() {
     //Create Chart and Render
     this.chart = new CanvasJS.Chart(this.chartContainerId, this.options)
-    // var point = this.datasets[0].points[this.options.lineAtIndex]
-    // var scale = this.scale
-    // this.chart.ctx.beginPath()
-    // this.chart.ctx.moveTo(point.x, scale.startPoint + 24)
-    // this.chart.ctx.strokeStyle = '#ff0000'
-    // this.chart.ctx.lineTo(point.x, scale.endPoint)
-    // this.chart.ctx.stroke()
-
-    // // write TODAY
-    // this.chart.ctx.textAlign = 'center'
-    // this.chart.ctx.fillText("TODAY", point.x, scale.startPoint + 12)
     this.chart.render()
 
     if (this.props.onRef) this.props.onRef(this.chart)
